07720c8e40986b01b6c89f4b589b89e5fe2549e4
Spencer Wise <delta.wiseman@gmail.com>
2019-07-26 05:34:26 -0500

    Img change

--- New folder/script.js
+++ New folder/script.js
@@ -1,19 +1,36 @@
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-
-}const button = document.querySelector('button');
+const button = document.querySelector('button');
 button.onclick = function() {
   let name = prompt('What is your name?');
 sssalert('Hello ' + name + ', nice to see you!');}
  
 
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
+// diretioncontrols
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+//  image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
