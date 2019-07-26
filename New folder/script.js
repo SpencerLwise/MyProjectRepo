fb9a62d8e0c2006df3ce999849ac39f5828b7344
Spencer Wise <delta.wiseman@gmail.com>
2019-07-26 00:57:21 -0500

    putting it together

--- New folder/script.js
+++ New folder/script.js
@@ -1,10 +1,8 @@
 
 const button = document.querySelector('button');
-
 button.onclick = function() {
   let name = prompt('What is your name?');
-
-  alert('Hello ' + name + ', nice to see you!');
+sssalert('Hello ' + name + ', nice to see you!');}
  
 
 
